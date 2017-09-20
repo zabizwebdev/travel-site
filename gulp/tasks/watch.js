5fbffa7a180a7b19d2a05121429e1487b02868e4
webdev@zabiz.net <webdev@zabiz.net>
2017-09-20 21:22:08 +0200

    Completed headers

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ var gulp = require('gulp'),
 watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
 
   browserSync.init({
     notify: false,
@@ -11,15 +11,25 @@ gulp.task('watch', function(){
     }
   });
 
-  watch('./app/index.html', function(){
+  watch('./app/index.html', function() {
     browserSync.reload();
   });
-  watch('./app/assets/styles/**/*.css', function(){
+
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject');
   });
+
+  watch('./app/assets/scripts/**/*.js', function() {
+    gulp.start('scriptsRefresh');
+  })
+
 });
 
-gulp.task('cssInject', ['styles'], function(){
+gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
 });
